fb794c532611ea31b09ac6da0f3c6f365f35d12c
Muhannad Elbolaky <major.amari@gmail.com>
2024-06-08 08:35:22 +0300

    update

---
+++ src/questions.ts
@@ -0,0 +1,44 @@
+const questionElement = document.getElementById(
+	"question-answer",
+) as HTMLDivElement;
+if (questionElement == null) throw new Error("Question element not found");
+
+import questions from "../questions.json";
+
+questions;
+
+for (let index = 0; index < questions.length; index++) {
+	const rawQuestion = questions[index];
+
+	const container = document.createElement("p");
+	container.classList.add("question-container");
+	questionElement.appendChild(container);
+
+	// add the number of the question
+	const questionNumber = document.createElement("p");
+	questionNumber.classList.add("question-number");
+	questionNumber.innerText = String(index + 1);
+	container.appendChild(questionNumber);
+
+	const url = new URL(window.location.href);
+
+	url.searchParams.delete("id");
+	const newUrl = url.origin + url.pathname + url.search;
+
+	questionNumber.addEventListener("click", () => {
+		navigator.clipboard.writeText(`${newUrl}#question${index + 1}`);
+		window.location.assign(`${newUrl}#question${index + 1}`);
+	});
+
+	const question = document.createElement("p");
+	question.classList.add("question-page");
+	question.id = `question${index + 1}`;
+	question.innerText = rawQuestion.question;
+	container.appendChild(question);
+
+	// add the answer
+	const answer = document.createElement("p");
+	answer.classList.add("answer");
+	answer.innerText = rawQuestion.options[0];
+	container.appendChild(answer);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
 			input: {
 				main: "index.html",
 				exam: "exam.html",
+				questions: "questions.html",
 			},
 		},
 	},
